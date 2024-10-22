3555e7e7bb1c4975bf25ccd08e8a01b017c1f4aa
who asks? <user@localhost>
2024-10-22 17:04:04 -0600

    fix

--- pelican/plugins/taskstack/taskstack.py
+++ pelican/plugins/taskstack/taskstack.py
@@ -139,7 +139,7 @@ class TaskStack:
             for event in issue.events():
                 if event.event == 'labeled' and event.label['name'] == 'WIP':
                     # start_time = event.created_at.astimezone(timezone.utc)
-                    start_time = datetime.fromisoformat(event.created_at)
+                    start_time = event.created_at
                     elapsed = (datetime.utcnow().astimezone(timezone.utc) - start_time).total_seconds() / 60
                     return min(100, (elapsed / self.pomodoro_duration) * 100)
             logger.warning(f'Progress for current pomodoro({issue.number}): {elapsed} minutes since {start_time}')
