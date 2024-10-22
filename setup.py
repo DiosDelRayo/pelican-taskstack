524a9177614d450664162751bf6e80372569ccfc
who asks? <user@localhost>
2024-10-22 15:36:03 -0600

    fix

--- setup.py
+++ setup.py
@@ -2,18 +2,11 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='pelican-taskstack',
-    version='0.2.9',
+    version='0.2.10',
     description='Pelican plugin for visualizing TaskStacks',
     author='DiosDelRayo',
     packages=find_namespace_packages(include=['pelican.*']),
     include_package_data=True,
-    package_data={
-        'pelican': [
-            'static/css/*.css',
-            'static/js/*.js',
-            'templates/*.html',
-        ],
-    },
     install_requires=[
         'github3.py>=3.0.0',
         'pelican>=4.0.0',
